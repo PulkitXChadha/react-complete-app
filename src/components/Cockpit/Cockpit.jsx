d920df1b82b81fce2eb68893b249246585db6500
puchadha <puchadha@adobe.com>
2020-03-26 15:14:07 -0400

    Update Cockpit.jsx

--- src/components/Cockpit/Cockpit.jsx
+++ src/components/Cockpit/Cockpit.jsx
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.css";
 
 const Cockpit = props => {
+  useEffect(() => {
+    console.log("[Cockpit.jsx] - useEffect");
+    //http: request
+    setTimeout(() => {
+      alert("Saved data to cloud!");
+    }, 1000);
+    return () => {
+      console.log("[Cockpit.jsx] - cleanUp work");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.jsx] - 2nduseEffect");
+    return () => {
+      console.log("[Cockpit.jsx] - clean Up work in 2nd useEffect");
+    };
+  });
+
   let assignedClasses = [];
 
   let btnClass = "";
   if (props.showPerson) {
     btnClass = classes.red;
   }
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red);
   }
 
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold);
   }
   return (
-    <div className={classes.Cockpit}>
-      <h1>{props.title}</h1>
-      <p className={assignedClasses.join(" ")}>This is really Working</p>
-      <button className={btnClass} onClick={props.clicked}>
-        Toggle Profiles
-      </button>
+    <div>
+      <div className={classes.Cockpit}>
+        <h1>{props.title}</h1>
+        <p className={assignedClasses.join(" ")}>This is really Working</p>
+        <button className={btnClass} onClick={props.clicked}>
+          Toggle Profiles
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Cockpit;
+export default React.memo(Cockpit);
